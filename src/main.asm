d888699621a812214fcddf7b7bdd10cefe43137e
Nikita Leontev <vkhk99@gmail.com>
2026-02-13 07:15:01 +0700

    Fixed sections order and make simple inserting of data into stack using esp

--- src/main.asm
+++ src/main.asm
@@ -1,10 +1,15 @@
-section .text
-
 section .data
+
+section .text
 
 global _start
 
 _start:
-    MOV ebx,42
+    SUB esp,16 ; Moving pointer 16 bytes
+    MOV dword [esp],42 ; Writing by address of moved pointer
+    MOV eax,[esp] ; Just putting data to eax to read from gdb
+
+    ; Exiting
+    MOV ebx,0
     MOV eax,1
     INT 80h
