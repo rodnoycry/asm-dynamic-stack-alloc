42f06b48b390907fc963bb0e2f68f604e4f5a8e9
Nikita Leontev <vkhk99@gmail.com>
2026-02-13 08:14:04 +0700

    First attempt to define static array allocation first

--- src/main.asm
+++ src/main.asm
@@ -1,13 +1,43 @@
 section .data
+    length dd 0
+    capacity dd 4
+    unit_size dd 4
 
 section .text
 
 global _start
 
+panic:
+    MOV ebx,1
+    MOV eax,1
+    INT 80h
+
+push_to_arr:
+    ; basic pushing logic
+    MOV eax,[length]
+    CMP eax,[capacity]
+    JE panic
+    MOV eax,[length]
+    INC eax
+    MUL dword [unit_size]
+    ADD eax,8
+    MOV ebx,[ebp + 8]
+    MOV dword [eax],ebx
+    RET
+
 _start:
-    SUB esp,16 ; Moving pointer 16 bytes
-    MOV dword [esp],42 ; Writing by address of moved pointer
-    MOV eax,[esp] ; Just putting data to eax to read from gdb
+    ; Allocating the memory
+    MOV eax,[capacity]
+    MUL dword [unit_size]
+    SUB esp,eax
+
+    ; Saving ebp
+    PUSH ebp
+    ; Caching esp value in ebp
+    MOV ebp,esp
+    ; Pushing new value
+    PUSH 42
+    CALL push_to_arr
 
     ; Exiting
     MOV ebx,0
